17d3a9248ce591fbefa9c636a72cc7e638135e2e
Madhuri8247 <madhurimandalapu82473@gmail.com>
2024-12-17 14:57:06 +0100

    Updated Movie,OrderRow with Required Properties

--- Models/DataBase/Movie.cs
+++ Models/DataBase/Movie.cs
@@ -1,10 +1,25 @@
-﻿namespace MovieShop.Models.DataBase
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieShop.Models.DataBase
 {
     public class Movie
     {
+        public int Id { get; set; }
 
+        [Required]
+        public string Title { get; set; }
 
+        [Required]
+        public string Director { get; set; }
 
+        [Required]
+        public int ReleaseYear { get; set; }
+
+        [Required]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+        public virtual ICollection<OrderRow> OrderRows { get; set; } = new List<OrderRow>();
 
     }
 }
--- Models/DataBase/OrderRow.cs
+++ Models/DataBase/OrderRow.cs
@@ -1,6 +1,22 @@
-﻿namespace MovieShop.Models.DataBase
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieShop.Models.DataBase
 {
     public class OrderRow
     {
+        public int Id { get; set; }
+
+        [Required]
+        public int OrderId { get; set; }
+
+        [Required]
+        public int MovieId { get; set; }
+
+        [Required]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+        public virtual Order Order { get; set; }
+        public virtual Movie Movie { get; set; }
     }
 }
