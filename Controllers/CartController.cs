1aa757945652025486e052eb9743a75318c30e8d
Michał Idczak <michal.idczak92@gmail.com>
2024-12-18 20:20:30 +0100

    Update

---
+++ Controllers/CartController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieShop.Data;
+using MovieShop.Models.DataBase;
+using MovieShop.Services;
+
+namespace MovieShop.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ICartService _cartService; 
+        private readonly MovieDbContext _db;
+        private readonly ILogger<CartController> _logger;
+        public CartController(ICartService cartService, MovieDbContext db, ILogger<CartController> logger)
+        {
+            _cartService = cartService;
+            _db = db;
+            _logger = logger;
+        }
+     
+        public IActionResult Index()
+        {
+            //var cartItems = _cartService.GetCartMovies(HttpContext.Session); // TODO : Add Session logic for cart.
+            //var totalPrice = _cartService.GetTotalPrice(HttpContext.Session);
+
+            //var model = new CartViewModel // TODO : Create View Model for CartViewModel
+            //{
+            //    Items = cartItems,
+            //    TotalPrice = totalPrice
+            //};
+
+            return View(); // TODO: return View(model) after fixes.
+        }
+        public IActionResult AddProductToCart(ISession session, Movie item) 
+        {
+ 
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult RemoveProductFromCart(ISession session, Movie item)
+        {
+       
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult EmptyCart(ISession session)
+        {
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Services/CartService.cs
@@ -0,0 +1,33 @@
+﻿using MovieShop.Models.DataBase;
+
+namespace MovieShop.Services
+{
+    public class CartService : ICartService
+    {
+
+        public void AddToCart(ISession session, Movie item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ClearCart(ISession session)
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal GetTotalPrice(ISession session)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveFromCart(ISession session, int movieId)
+        {
+            throw new NotImplementedException();
+        }
+
+        List<Movie> ICartService.GetCartMovies(ISession session)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
