7493b500b1d8004f8e6847fa262ccdad20e43bc7
Michał Idczak <michal.idczak92@gmail.com>
2024-12-18 09:31:43 +0100

    error update DbContext, Program.cs

--- Program.cs
+++ Program.cs
@@ -10,12 +10,13 @@ public class Program
         var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
             ?? throw new AggregateException("Default connection not found");
 
-        builder.Services.AddDbContext<MovieDbContext>(opt => opt.UseSqlServer(connectionString));
-
+        builder.Services.AddDbContext<MovieDBContext>(opt => opt.UseSqlServer(connectionString));
+       
         // Add services to the container.
         builder.Services.AddControllersWithViews();
         //builder.Services.AddScoped<IMovieService, MovieService>(); // implementation of service
         var app = builder.Build();
+
 
         // Configure the HTTP request pipeline.
         if (!app.Environment.IsDevelopment())
