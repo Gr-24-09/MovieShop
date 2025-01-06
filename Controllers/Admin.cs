96a37ffc5bdf6965f5726b80a141432d7bc2b170
Basel <basel.eibo4676@gmail.com>
2025-01-06 21:16:59 +0100

    creating Admin controller , and created an orders action .

---
+++ Controllers/Admin.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MovieShop.Data;
+
+namespace MovieShop.Controllers
+{
+    public class Admin : Controller
+    {
+        private readonly MovieDbContext _db;
+
+        public Admin(MovieDbContext movieDbContext)
+        {
+            _db = movieDbContext;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Orders()
+        {
+            var orders = _db.Orders
+                .Include(o => o.OrderRows)
+                .ThenInclude(or => or.Movie)
+                .Include(o => o.Customer)
+                .OrderByDescending(o => o.OrderDate)
+                .Select(o => new
+                {
+                    OrderId = o.Id,
+                    CustomerName = $"{o.Customer.FirstNameBillingAddress} {o.Customer.LastNameBillingAddress}",
+                    OrderDate = o.OrderDate,
+                    TotalCost = o.OrderRows.Sum(or => or.Quantity * or.Price),
+                    Movies = o.OrderRows.Select(or => new
+                    {
+                        or.Movie.Title,
+                        or.Quantity,
+                        or.Price
+                    })
+                })
+                .ToList();
+
+            return View(orders);
+        }
+    }
+}
