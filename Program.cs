a850d5f195c96437dc495144c42e0a603be68351
Basel <basel.eibo4676@gmail.com>
2025-01-07 00:47:42 +0100

    .

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ public class Program
         builder.Services.AddControllersWithViews();
         builder.Services.AddScoped<IMovieService, MovieService>(); // implementation of Movie Service
         builder.Services.AddScoped<ICartService, CartService>(); // implementation of Cart Service
-        //builder.Services.AddScoped<TMDBService>(); // Service that updates posters path in database
+        builder.Services.AddScoped<TMDBService>(); // Service that updates posters path in database
         builder.Services.AddScoped<ICustomerService, CustomerService>(); // implementation of Customer Service
 
 
@@ -56,7 +56,15 @@ public class Program
         app.MapControllerRoute(
             name: "default",
             pattern: "{controller=Home}/{action=Index}/{id?}");
- 
+
+        //Part of TMDBService - when WebShop is loaded, update poster path in database(image for movie)
+        //app.Lifetime.ApplicationStarted.Register(async () =>
+        //{
+        //    using var scope = app.Services.CreateScope();
+        //    var tmdbService = scope.ServiceProvider.GetRequiredService<TMDBService>();
+        //    await tmdbService.UpdatePosterPathsAsync();
+        //    Console.WriteLine("Download success.");
+        //});
         ////Part of TMDBService - when WebShop is loaded, update poster path in database(image for movie)
         //    app.Lifetime.ApplicationStarted.Register(async () =>
         //    {
@@ -65,14 +73,6 @@ public class Program
         //        await tmdbService.UpdatePosterPathsAsync();
         //        Console.WriteLine("Download success.");
         //    });
-        // Part of TMDBService - when WebShop is loaded, update poster path in database (image for movie)
-        //app.Lifetime.ApplicationStarted.Register(async () =>
-        //{
-        //    using var scope = app.Services.CreateScope();
-        //    var tmdbService = scope.ServiceProvider.GetRequiredService<TMDBService>();
-        //    await tmdbService.UpdatePosterPathsAsync();
-        //    Console.WriteLine("Download success.");
-        //});
 
         app.Run();
     }
